3728982488f0ea5816e714b86de2521101ff0ec6
Unknown <omer531998@gmail.com>
2018-02-02 22:04:09 +0200

    basic file upload

--- main.js
+++ main.js
@@ -30,47 +30,32 @@ function setReqOptions(endpoint, inputfile) { //TODO
     return options;
 }
 
-app.use(express.static(path.join(__dirname, 'public')));
+function get_video_url(file_path) {
+    return "http://im_a_url.look.at_me"
+}
 
-app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname, 'views/index.html'));
-});
+http.createServer(function (req, res) {
+    if (req.url == '/fileupload') {
+        var form = new formidable.IncomingForm();
+        form.parse(req, function (err, fields, files) {
+            var file_path = files.filetoupload.path;
+            console.log("File uploaded to path:", file_path);
+            var url = get_video_url();
+            res.write("video url: ");
+            res.write(url);
+            res.end();
+        });
+    } else {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+        res.write('<input type="file" name="filetoupload"><br>');
+        res.write('<input type="submit">');
+        res.write('</form>');
+        return res.end();
+    }
+}).listen(8080);
 
-app.post('/upload', function(req, res){
-
-    // create an incoming form object
-    var form = new formidable.IncomingForm();
-
-    // specify that we want to allow the user to upload multiple files in a single request
-    form.multiples = true;
-
-    // store all uploads in the /uploads directory
-    form.uploadDir = path.join(__dirname, '/uploads');
-
-    // every time a file has been uploaded successfully,
-    // rename it to it's orignal name
-    form.on('file', function(field, file) {
-        fs.rename(file.path, path.join(form.uploadDir, file.name));
-    });
-
-    // log any errors that occur
-    form.on('error', function(err) {
-        console.log('An error has occured: \n' + err);
-    });
-
-    // once all the files have been uploaded, send a response to the client
-    form.on('end', function() {
-        res.end('success');
-    });
-
-    // parse the incoming request containing the form data
-    form.parse(req);
-
-});
-
-var server = app.listen(3000, function(){
-    console.log('Server listening on port 3000');
-});
+console.log("Server listening on port 8080");
 
 /*
 //make request for JSON
@@ -89,6 +74,3 @@ app.get('/img/bg.png', function(req, res) {
 
 */
 
-console.log("Express app running on port 3000");
-
-module.exports = app;
