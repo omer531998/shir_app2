e4a7e79bf1ab01068e8a94f53129326207827b83
Unknown <omer531998@gmail.com>
2018-02-03 04:21:47 +0200

    add consts

--- main.js
+++ main.js
@@ -2,20 +2,21 @@ var formidable = require('formidable');
 var fs = require('fs');
 var http = require("http");
 
+const SERVER_PORT = 8080;
 
 function get_video_url(file_path, end_point) {
     return "http://im_a_url.look.at_me"
 }
 
 http.createServer(function (req, res) {
-    if (req.url == '/fileupload') {
+    if (req.url === '/file_upload') {
         var form = new formidable.IncomingForm();
         form.parse(req, function (err, fields, files) {
             var file_path = files.file_to_upload.path;
             var end_point = fields.end_point;
             console.log("File uploaded to path:", file_path);
             var url = get_video_url(file_path, end_point);
-            res.write("video url: ");
+            res.write("video url: "); // Or return a file
             res.write(url);
             res.end();
         });
@@ -32,9 +33,9 @@ http.createServer(function (req, res) {
         });
 
     }
-}).listen(8080);
+}).listen(SERVER_PORT);
 
-console.log("Server listening on port 8080");
+console.log("Server listening on port: " + SERVER_PORT);
 
 /*
 //make request for JSON
