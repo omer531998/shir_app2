e4fb128ece5b73ce191471a4fd705bfe7744b4b4
Unknown <omer531998@gmail.com>
2018-02-03 16:06:58 +0200

    added get_output_file

--- main.js
+++ main.js
@@ -1,43 +1,63 @@
 var formidable = require('formidable');
 var fs = require('fs');
 var http = require("http");
+var request = require("request");
 
 const SERVER_PORT = 8080;
 
-function get_video_url(file_path, end_point) {
-    return "http://im_a_url.look.at_me"
+function get_output_file(file, end_point) {
+    end_point = end_point.replace("create_video_session", "create_video_spec");
+    var file_data = fs.readFile(file.path, function(err, data){
+        if(err){
+            console.log("Error opening file: " + file.path);
+        } else {
+            return data;
+        }
+    });
+
+    request.post({
+        headers: {'content-type' : 'application/x-www-form-urlencoded'},
+        url:     end_point,
+        body:    file_data
+    }, function(error, response, body){
+        console.log(body);
+    });
+    return body;
 }
 
-http.createServer(function (req, res) {
-    if (req.url === '/file_upload') {
-        var form = new formidable.IncomingForm();
-        form.parse(req, function (err, fields, files) {
-            var file_path = files.file_to_upload.path;
-            var end_point = fields.end_point;
-            console.log("File uploaded to path:", file_path);
-            var url = get_video_url(file_path, end_point);
-            res.write("video url: "); // Or return a file
-            res.write(url);
-            res.end();
-        });
-    } else {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        fs.readFile("index.html", function(err, data){
-            if(err){
-                res.statusCode = 500;
-                res.end('Error getting the file: ${err}.');
-            } else {
-                res.write(data);
+
+function main() {
+    http.createServer(function (req, res) {
+        if (req.url === '/file_upload') {
+            var form = new formidable.IncomingForm();
+            form.multiples = true;
+            form.parse(req, function (err, fields, form_files) {
+                var input_files = form_files.files_to_upload;
+                var end_point = fields.end_point;
+                var output_files = input_files.map(function(input_file) {
+                    return get_output_file(input_file, end_point);
+                });
+                // TODO: add all files to a rar file
                 res.end();
-            }
-        });
+            });
+        } else {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            fs.readFile("index.html", function(err, data){
+                if(err){
+                    res.statusCode = 500;
+                    res.end('Error getting the file: ' + err);
+                } else {
+                    res.write(data);
+                    res.end();
+                }
+            });
+        }
+    }).listen(SERVER_PORT);
 
-    }
-}).listen(SERVER_PORT);
+    console.log("Server listening on port: " + SERVER_PORT);
 
-console.log("Server listening on port: " + SERVER_PORT);
 
-/*
+    /*
 //make request for JSON
 inputfiles.forEach(function(inputfiles){
     var reqOptions = setReqOptions(endpoint, inputfile);
@@ -53,4 +73,6 @@ app.get('/img/bg.png', function(req, res) {
 })
 
 */
+}
 
+main()
